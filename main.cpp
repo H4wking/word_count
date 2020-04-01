80ae0611d488abbcf1206bda151063b71025e3c4
Sluzhynskyi <sluzhynskyi@gmail.com>
2020-04-01 18:07:56 +0200

    with writing UTF8

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 #include <locale>
 #include <boost/algorithm/string.hpp>
 
-
 static void extract_to_vector(const std::string &buffer, std::vector<std::string> *vector_txt) {
     struct archive_entry *entry;
     int r;
@@ -29,37 +28,39 @@ static void extract_to_vector(const std::string &buffer, std::vector<std::string
 //    archive_read_free(a);
 }
 
-void write_file(std::string filename, std::map<std::string, int> mp){
 
-    FILE *f = fopen(filename.c_str(), "w");
-
-    for(std::map<std::string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
-        fprintf(f, "%s  %d\n", it->first.c_str(), it->second);
+int write_file(std::string filename, std::map<std::string, int> mp) {
+    std::wofstream out;
+    out.open(filename, std::ios::out | std::ios::app);
+    out.imbue(std::locale("en_US.UTF-8"));
+    for (auto &it : mp) {
+        out << it.first.c_str() << "  " << it.second << std::endl;
     }
-    fclose(f);
+    out.close();
+    return 0;
 }
 
-void write_file2(std::string filename, std::vector<std::pair<std::string, int>> vec){
-    FILE *f = fopen(filename.c_str(), "w");
-
-    for ( std::vector<std::pair<std::string, int>>::const_iterator it = vec.begin() ; it != vec.end(); it++) {
-        fprintf(f, "%s  %d\n", it->first.c_str(), it->second);
+int write_file2(std::string filename, std::vector<std::pair<std::string, int>> vec) {
+    std::wofstream out;
+    out.open(filename, std::ios::out | std::ios::app);
+    out.imbue(std::locale("en_US.UTF-8"));
+    for (const auto &it : vec) {
+        out << it.first.c_str() << "  " << it.second << std::endl;
     }
-
-    fclose(f);
+    out.close();
+    return 0;
 }
 
-bool sortByVal(const std::pair <std::string, int> &a, const std::pair<std::string, int> &b){
+bool sortByVal(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b) {
     return (a.second > b.second);
 }
 
-std::vector<std::pair<std::string, int>> sort_by_value (std::map<std::string,int> mp){
-    std::map<std::string, int> :: iterator it;
+std::vector<std::pair<std::string, int>> sort_by_value(std::map<std::string, int> mp) {
+    std::map<std::string, int>::iterator it;
 
     std::vector<std::pair<std::string, int>> vec;
 
-    for (it=mp.begin(); it!=mp.end(); it++)
-    {
+    for (it = mp.begin(); it != mp.end(); it++) {
         vec.push_back(make_pair(it->first, it->second));
     }
 
@@ -68,8 +69,7 @@ std::vector<std::pair<std::string, int>> sort_by_value (std::map<std::string,int
 
     // print the vector
     std::cout << "The map, sorted by value is: " << std::endl;
-    for (int i = 0; i < vec.size(); i++)
-    {
+    for (int i = 0; i < vec.size(); i++) {
         std::cout << vec[i].first << ": " << vec[i].second << std::endl;
     }
 
